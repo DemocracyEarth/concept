de41f7e14f2b6fb526ea401b0d34bf123fddfba6
says33 <says.rodriguez@gmail.com>
2016-06-30 11:02:54 -0500

    #master se agrega el rout para el metodo lgout

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
    post '/auth', to: 'users#login', as: 'authenticated'
    get '/vote', to: 'users#vote', as: 'vote'
    get '/decition', to: 'users#decition', as: 'decition'
-   get 'confirm', to: 'users#confirmVote', as: 'confirm'
+   get '/confirm', to: 'users#confirmVote', as: 'confirm'
+   get '/logout', to: 'users#logout', as: 'singout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
